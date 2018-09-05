50a72258a85bc57e68bce6de9665924a0737e354
José Freitas <40635205+archifreitas@users.noreply.github.com>
2018-09-05 23:23:48 +0100

    missing 2 memories

--- assets/js/index.js
+++ assets/js/index.js
@@ -62,3 +62,12 @@ function hideShow3() {
         x.style.display = "none";
     }
 }
+
+function hideShow4() {
+    var x = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
