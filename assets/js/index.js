a211aea4a95aac99a1e8bd7d9935faa4887e1ce5
José Freitas <40635205+archifreitas@users.noreply.github.com>
2018-09-10 12:31:51 +0100

    update project arrows

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,47 +27,77 @@ window.onload = function(){
   }
 };
 
-function hideShow() {
-    var x = document.getElementById("myDIV");
+function hideShow0() {
+    var x = document.getElementById("myDIV0");
+    var y = document.getElementById("myDIV0.1");
+    var z = document.getElementById("myDIV0.2");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
     }
 }
 
 function hideShow1() {
     var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV1.1");
+    var z = document.getElementById("myDIV1.2");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
     }
 }
 
 function hideShow2() {
     var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV2.1");
+    var z = document.getElementById("myDIV2.2");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
     }
 }
 
 function hideShow3() {
     var x = document.getElementById("myDIV3");
+    var y = document.getElementById("myDIV3.1");
+    var z = document.getElementById("myDIV3.2");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
     }
 }
 
 function hideShow4() {
     var x = document.getElementById("myDIV4");
+    var y = document.getElementById("myDIV4.1");
+    var z = document.getElementById("myDIV4.2");
     if (x.style.display === "none") {
         x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
     }
 }
